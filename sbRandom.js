26af9c3a269a139c683cf6533f083ed24eac422c
Sajni Bercel <SajniBercel@gmail.com>
2025-10-08 02:01:35 +0200

    only localhost from now, own random number
    cuz of the import export system the page only works on local host from now, own rnd with seed, so from the same seed produse the same secvense of numbers (LSC pseudo random)

---
+++ sbRandom.js
@@ -0,0 +1,11 @@
+let s = 1;
+
+export function seed(x) {
+    s = (x >>> 0) || 1;
+}
+
+export function rand() {
+    //s = (s * 1664525 + 1013904223) >>> 0;
+    s = (s * 75) % 65537;
+    return s;
+}
